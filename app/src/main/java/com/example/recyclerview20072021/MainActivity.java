231c39f5a4b79604c74c8dcadaea78e2e71925cd
phatdroid94@gmail.com <phatdroid94@gmail.com>
2021-08-26 19:30:10 +0700

    gan adapter cho recycler

--- app/src/main/java/com/example/recyclerview20072021/MainActivity.java
+++ app/src/main/java/com/example/recyclerview20072021/MainActivity.java
@@ -1,14 +1,30 @@
 package com.example.recyclerview20072021;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
+    RecyclerView mRcvFood;
+    List<Food> mListFood;
+    FoodAdapter mFoodAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mRcvFood = findViewById(R.id.recyclerView);
+        mListFood = Food.getMock();
+        mFoodAdapter = new FoodAdapter(mListFood);
+
+        mRcvFood.setLayoutManager(new LinearLayoutManager(this));
+        mRcvFood.setHasFixedSize(true);
+        mRcvFood.setAdapter(mFoodAdapter);
     }
 }
